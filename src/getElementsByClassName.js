be3bc004b69cc89f104c1efdcb4e947d74a7c9d1
burkegg <burkegreen@gmail.com>
2018-06-11 12:15:46 -0700

    Complete getElementsByClassName and pass all tests

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,31 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  // your code here
-};
+var getElementsByClassName = function(className) {
+  var storage = [];
+  
+  // recursive helper func that checks if el includes className
+  var helper = function(element) {
+    // check if element is not undefined
+    if (element.classList) {
+      if (element.classList.contains(className)) {
+        // console.log(element);
+        storage.push(element); 
+      }
+      // console.log('storage: ',  storage);
+      // call helper on childNodes of elements
+      if (element.hasChildNodes()) {
+        var children = element.childNodes; 
+
+        for (var i = 0; i < children.length; i++) {
+          helper(children[i]);
+        }
+      } 
+    }     
+  };
+  
+  helper(document.body);
+  // console.log('final storage: ', storage);
+  return storage;  
+}; 
+
